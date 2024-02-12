2d2d4b2d41f62b1739867e3ff0db4fe860f23511
minidu2003 <158209144+minidu2003@users.noreply.github.com>
2024-02-12 21:30:42 +0530

    Update teacher-table.component.ts

--- frontend/src/app/components/teacher-table/teacher-table.component.ts
+++ frontend/src/app/components/teacher-table/teacher-table.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router, NavigationExtras } from '@angular/router';
 import { faTrash, faPlus, faPenSquare } from '@fortawesome/free-solid-svg-icons';
 import { AppServiceService } from '../../app-service.service';
+
 @Component({
   selector: 'app-teacher-table',
   templateUrl: './teacher-table.component.html',
@@ -22,7 +23,7 @@ export class TeacherTableComponent implements OnInit {
   }
 
   addNewTeacher() {
-    this.router.navigate(['addTeacher'])
+    this.router.navigate(['addTeacher']);
   }
 
   editTeacher(id) {
@@ -31,24 +32,24 @@ export class TeacherTableComponent implements OnInit {
         id: id
       }
     };
-    this.router.navigate(['editTeacher'], navigationExtras)
+    this.router.navigate(['editTeacher'], navigationExtras);
   }
 
-  initializeDB(){
+  initializeDB() {
     this.service.initializeDB().subscribe((response) => {
-      console.log('DB is Initialized')
+      console.log('DB is Initialized');
     }, (error) => {
-      console.log('ERROR - ', error)
-    })
+      console.log('ERROR - ', error);
+    });
   }
 
   getTeacherData() {
     this.selected = 'Teachers';
     this.service.getTeacherData().subscribe((response) => {
-      this.teacherData = Object.keys(response).map((key) => [response[key]]);
+      this.teacherData = Object.keys(response).map((key) => response[key]);
     }, (error) => {
-      console.log('ERROR - ', error)
-    })
+      console.log('ERROR - ', error);
+    });
   }
 
   getStudentData() {
@@ -56,31 +57,28 @@ export class TeacherTableComponent implements OnInit {
     this.service.getStudentData().subscribe((response) => {
       this.teacherData = response;
     }, (error) => {
-      console.log('ERROR - ', error)
-    })
+      console.log('ERROR - ', error);
+    });
   }
 
   search(value) {
-    let foundItems = [];
-    if (value.length <= 0) {
-      this.getStudentData();
-    } else {
-      let b = this.studentData.filter((student) => {
-        if (student.name.toLowerCase().indexOf(value.toLowerCase()) > -1) {
-          foundItems.push(student);
-        }
-      });
-      this.studentData = foundItems;
+    if (!value || value.trim() === '') {
+      this.getTeacherData();
+      return;
     }
-  }
 
+    const searchValue = value.trim().toLowerCase();
+    this.teacherData = this.teacherData.filter((teacher) =>
+      teacher.name.toLowerCase().includes(searchValue)
+    );
+  }
 
   deleteTeacher(itemid) {
     const test = {
       id: itemid
-    }
+    };
     this.service.deleteTeacher(test).subscribe((response) => {
-      this.getTeacherData()
-    })
+      this.getTeacherData();
+    });
   }
 }
