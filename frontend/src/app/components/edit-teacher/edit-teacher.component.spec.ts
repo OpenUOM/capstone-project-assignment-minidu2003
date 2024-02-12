4ab1984fd6c68dd7e95b2ca0faa6393d859dd7c7
minidu2003 <158209144+minidu2003@users.noreply.github.com>
2024-02-12 20:37:54 +0530

    Update edit-teacher.component.spec.ts

--- frontend/src/app/components/edit-teacher/edit-teacher.component.spec.ts
+++ frontend/src/app/components/edit-teacher/edit-teacher.component.spec.ts
@@ -22,4 +22,28 @@ describe('EditTeacherComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+});import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { EditTeacherComponent } from './edit-teacher.component';
+
+describe('EditTeacherComponent', () => {
+  let component: EditTeacherComponent;
+  let fixture: ComponentFixture<EditTeacherComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ EditTeacherComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(EditTeacherComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
 });
